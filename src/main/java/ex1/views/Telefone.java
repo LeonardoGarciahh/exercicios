803a04aa35f4e6720f3b2701711bbb348e9c3394
LEONARDO GARCIA DE OLIVEIRA <leonardo.oliveira7@alunos.sc.senac.br>
2022-03-31 09:23:32 -0300

    changed input to combobox

--- src/main/java/ex1/views/Telefone.java
+++ src/main/java/ex1/views/Telefone.java
@@ -1,7 +1,9 @@
 package ex1.views;
 
+import ex1.controller.ClienteController;
 import ex1.controller.LinhaTelefonicaController;
 import ex1.controller.PhoneController;
+import ex1.model.vo.ClienteVO;
 import ex1.model.vo.LinhaTelefonicaVO;
 import ex1.model.vo.PhoneVO;
 
@@ -9,6 +11,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Telefone extends JFrame{
     private JTextField ddiField;
@@ -17,24 +20,31 @@ public class Telefone extends JFrame{
     private JTextField numeroField;
     private JTextField tipoField;
     private JButton cadastrarButton;
+    private JComboBox clientesBox;
     private JTextField idclienteField;
 
-    public Telefone() {
+    public Telefone() throws SQLException {
         setContentPane(panel1);
         setSize(500,150);
+        ArrayList<ClienteVO> clients = new ArrayList<ClienteVO>();
+        ClienteController clienteController = new ClienteController();
+        clients = clienteController.findAllClients();
+        for(int c = 0;c<clients.size();c++){
+            clientesBox.addItem(clients.get(c).getId()+" - "+clients.get(c).getName());
+        }
 
         cadastrarButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if(!ddiField.getText().trim().equals("") && !dddField.getText().trim().equals("") &&
-                    !numeroField.getText().trim().equals("") && !tipoField.getText().trim().equals("") &&
-                    !idclienteField.getText().trim().equals("") ){
+                    !numeroField.getText().trim().equals("") && !tipoField.getText().trim().equals("") ){
+                    int id = Integer.parseInt(clientesBox.getSelectedItem().toString().split("-")[0].trim());
 
                     PhoneVO phone = new PhoneVO(numeroField.getText(),Integer.parseInt(dddField.getText()),Integer.parseInt(ddiField.getText()),Integer.parseInt(tipoField.getText()));
                     PhoneController phoneController = new PhoneController();
                     LinhaTelefonicaController linhaTelefonicaController = new LinhaTelefonicaController();
                     try {
                         phone = phoneController.addPhone(phone);
-                        LinhaTelefonicaVO linhaTelefonicaVO = new LinhaTelefonicaVO(Integer.parseInt(idclienteField.getText()),phone.getId());
+                        LinhaTelefonicaVO linhaTelefonicaVO = new LinhaTelefonicaVO(id,phone.getId());
                         linhaTelefonicaController.associateWithClientAndPhone(linhaTelefonicaVO);
                     } catch (SQLException ex) {
                         ex.printStackTrace();
@@ -44,7 +54,7 @@ public class Telefone extends JFrame{
             }
         });
     }
-    public static void showScreen(){
+    public static void showScreen() throws SQLException {
         Telefone telefone = new Telefone();
         telefone.setResizable(false);
         telefone.setLocationRelativeTo(null);
