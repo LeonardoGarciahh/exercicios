d110a8438d001d68c96afe38fde9ed8c65865a4e
LEONARDO GARCIA DE OLIVEIRA <leonardo.oliveira7@alunos.sc.senac.br>
2022-05-09 10:53:26 -0300

    creating jmenu

--- src/main/java/ex1/views/Menu.java
+++ src/main/java/ex1/views/Menu.java
@@ -3,11 +3,15 @@ package ex1.views;
 import ex1.model.vo.ClienteVO;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
 public class Menu extends JFrame{
+
+    private JPanel contentPane;
     private JButton telefoneButton;
     private JPanel panel;
     private JButton associarTelefoneAoClienteButton;
@@ -15,33 +19,44 @@ public class Menu extends JFrame{
     private JButton allPhonesBtn;
     public static Menu menuFrame;
 
+
     public Menu() {
+        setSize(1000,300);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu mnNewMenu = new JMenu("New menu");
+        menuBar.add(mnNewMenu);
+
+        JMenuItem mntmNewMenuItem = new JMenuItem("Telefone");
+        mnNewMenu.add(mntmNewMenuItem);
+
+        JMenuItem mntmNewMenuItem_1 = new JMenuItem("New menu item");
+        mnNewMenu.add(mntmNewMenuItem_1);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
         setContentPane(panel);
-        setSize(400,150);
 
-
-
-        telefoneButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Telefone.showScreen();
-
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
+        mntmNewMenuItem.addActionListener(e -> {
+            Telefone telefone = new Telefone();
+            setContentPane(telefone);
+            revalidate();
         });
 
-        associarTelefoneAoClienteButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    AssociatePhoneAndClient.showScreen();
+        telefoneButton.addActionListener(e -> {
 
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
+        });
+
+        associarTelefoneAoClienteButton.addActionListener(e -> {
+            try {
+                AssociatePhoneAndClient.showScreen();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         });
         allClientsBtn.addActionListener(new ActionListener() {
@@ -70,16 +85,12 @@ public class Menu extends JFrame{
 
     public static void showScreen() throws SQLException {
         menuFrame = new Menu();
-        menuFrame.setResizable(false);
-
         menuFrame.setLocationRelativeTo(null);
         menuFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         menuFrame.setVisible(true);
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
+
 }
 
 
--- src/main/java/ex1/views/Telefone.java
+++ src/main/java/ex1/views/Telefone.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
-public class Telefone extends JFrame{
+public class Telefone extends JPanel{
     private JTextField ddiField;
     private JPanel panel1;
     private JTextField dddField;
@@ -16,23 +16,15 @@ public class Telefone extends JFrame{
     private JButton cadastrarButton;
     private JComboBox typeBox;
 
-    public Telefone() throws SQLException {
-        setContentPane(panel1);
-        setSize(500,90);
+    public Telefone() {
+        this.add(panel1);
         typeBox.addItem("Movel");
         typeBox.addItem("Fixo");
-//        ArrayList<ClienteVO> clients = new ArrayList<ClienteVO>();
-//        ClienteController clienteController = new ClienteController();
-//        clients = clienteController.findAllClients();
-//        for(int c = 0;c<clients.size();c++){
-//            clientesBox.addItem(clients.get(c).getId()+" - "+clients.get(c).getName());
-//        }
 
         cadastrarButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if(!ddiField.getText().trim().equals("") && !dddField.getText().trim().equals("") &&
                     !numeroField.getText().trim().equals("")){
-//                    int id = Integer.parseInt(clientesBox.getSelectedItem().toString().split("-")[0].trim());
 
                     PhoneVO phone = new PhoneVO(numeroField.getText(),Integer.parseInt(dddField.getText()),Integer.parseInt(ddiField.getText()),((typeBox.getSelectedItem() == "Movel")?0:1));
                     PhoneController phoneController = new PhoneController();
@@ -42,16 +34,9 @@ public class Telefone extends JFrame{
                         ex.printStackTrace();
                     }
                 }
-
             }
         });
-    }
-    public static void showScreen() throws SQLException {
-        Telefone telefone = new Telefone();
-        telefone.setResizable(false);
-        telefone.setLocationRelativeTo(null);
-//        telefone.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        telefone.setVisible(true);
 
+        this.setVisible(true);
     }
 }
